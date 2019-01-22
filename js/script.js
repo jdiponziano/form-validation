e633be9fceed3526024704e5ac58cdd5e9fb67a1
Jessica Di Ponziano <jdiponziano@gmail.com>
2019-01-21 21:07:43 -0500

    Hide other job title on load

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-$(function () {
+$(function() {
   //Variables
   const name = $('#name');
-  
+  const otherJobTitle = $('#other-title');
   //Focus on first field on load
   name.focus();
+  //Hide field on load
+  otherJobTitle.hide();
 });
