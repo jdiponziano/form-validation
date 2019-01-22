2d3f89c852e10f4fbc95b8162cad5456e4fcbdf4
Jessica Di Ponziano <jdiponziano@gmail.com>
2019-01-21 21:18:38 -0500

    Show hide other job title field

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 $(function() {
   //Variables
   const name = $('#name');
+  const title = $('#title');
   const otherJobTitle = $('#other-title');
+
   //Focus on first field on load
   name.focus();
+
   //Hide field on load
   otherJobTitle.hide();
+
+  title.on('change', () => {
+    selectValue = title.val();
+    if (selectValue == 'other') {
+      otherJobTitle.fadeIn();
+    } else {
+      otherJobTitle.fadeOut();
+    }
+  });
 });
