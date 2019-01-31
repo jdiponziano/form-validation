ad60842bda3df44e34c017f5d6e5193419605003
Jessica Di Ponziano <jess@webdevjd.com>
2019-01-30 21:44:11 -0500

    Add course disabling based on times

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ $(function() {
   const $activities = $('.activities');
   const $checkboxes = $('input[type=checkbox]');
   const $paymentSelect = $('#payment');
+  let $cost = 0;
+  let costString =
+    '<p>Total Cost: $<span class="cost">' + $cost + '</span></p>';
+
+  $activities.append(costString);
 
   //Focus on first field on load
   $name.focus();
@@ -44,9 +49,6 @@ $(function() {
   });
 
   //Sum up cost of workshops and display total
-  let $cost = 0;
-  let costString = '<p>Total Cost: $<span class="cost">' + $cost + '</span></p>';
-  $activities.append(costString);
   $checkboxes.on('change', e => {
     const $target = $(e.target);
     const $course = $target.attr('name');
@@ -65,6 +67,34 @@ $(function() {
       }
     }
     $costText.text($cost);
+  });
+
+  $checkboxes.on('change', e => {
+    const $target = $(e.target);
+    const $content = $target.parent().text();
+    const $stringTest = $target.parent().siblings('label');
+    const regex = /— (\w*) \d[a|p]m-\d\d?pm/;
+    const tuesday1 = /Tuesday 9am-12pm/i;
+    const tuesday2 = /Tuesday 1pm-4pm/i;
+    console.log($content.match(regex));
+    $stringTest.each(function(index) {
+      let $this = $(this);
+      let $optionText = $this.text();
+      let $optionInput = $this.find('input');
+      if (tuesday1.test($content) && tuesday1.test($optionText)) {
+        if ($target.is(':checked')) {
+          $optionInput.prop('disabled', 'true');
+        } else {
+          $optionInput.removeAttr('disabled');
+        }
+      } else if (tuesday2.test($content) && tuesday2.test($optionText)) {
+        if ($target.is(':checked')) {
+          $optionInput.prop('disabled', 'true');
+        } else {
+          $optionInput.removeAttr('disabled');
+        }
+      }
+    });
   });
 
   $paymentSelect.on('change', () => {
